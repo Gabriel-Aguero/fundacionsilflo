8bbe1ad14c13496c25fa6d650890e6f6fbd5d4f7
user.name: gabrielhaguero@gmail.com <gabrielhaguero@gmail.com>
2024-05-12 02:08:40 -0300

    actulizando repositorio

---
+++ .vercel/output/functions/_render.func/media/gaby/GABY/Programacion/Proyectos/fundacionsilflo/.vercel/output/_functions/chunks/capacitaciones_CTgEmiVk.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/capacitaciones_ie_pUf2P.mjs').then(n => n.f);
+
+export { page };
---
+++ .vercel/output/static/_astro/hoisted.Bh8oLAEj.js
@@ -0,0 +1,1 @@
+const e=document.getElementById("menu-button"),n=document.getElementById("menu-open");e.addEventListener("click",()=>{n.classList.toggle("hidden")});
