79e520d621cebf815a810f49e6d7a0114645ccf2
user.name: gabrielhaguero@gmail.com <gabrielhaguero@gmail.com>
2024-05-12 04:39:48 -0300

    subiendo archivos para despliegue

--- .vercel/output/functions/_render.func/media/gaby/GABY/Programacion/Proyectos/fundacionsilflo/.vercel/output/_functions/chunks/index_iSInRo0i.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_zu4usbdw.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/media/gaby/GABY/Programacion/Proyectos/fundacionsilflo/.vercel/output/_functions/chunks/la-fundacion_vtxTrZZt.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/la-fundacion_B3T_6VwF.mjs').then(n => n.l);
-
-export { page };
