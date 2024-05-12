a5df1b6b683e40827446cc595930ff1bc40e6488
user.name: gabrielhaguero@gmail.com <gabrielhaguero@gmail.com>
2024-05-12 03:09:38 -0300

    actulizando archivos nuevos

--- .vercel/output/functions/_render.func/media/gaby/GABY/Programacion/Proyectos/fundacionsilflo/.vercel/output/_functions/chunks/index_BFRUZxBA.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Df0KpGjP.mjs');
-
-export { page };
--- .vercel/output/static/_astro/hoisted.Bh8oLAEj.js
+++
@@ -1,1 +0,0 @@
-const e=document.getElementById("menu-button"),n=document.getElementById("menu-open");e.addEventListener("click",()=>{n.classList.toggle("hidden")});
